00a952635e80e55858d2e72c2acea953f122c9b3
Jallen Portugal <jallenqweqwe123@gmail.com>
2025-01-14 19:25:17 +0800

    added design to admin_confirmation

--- Frontend/html/admin_confirmation.php
+++ Frontend/html/admin_confirmation.php
@@ -57,12 +57,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="../css/gymApplicationConfirmation.css"> <!-- Optional CSS -->
 </head>
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    body{
+        background-color: #f0f0f0;
+        font-family: Poppins;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+    h1{
+        text-align: center;
+        color: #fab12f;
+    }
+    p{
+        text-align: center;
+        font-size: 1.2rem;
+    }
+    a button{
+        margin: auto;
+        padding: 15px 20px;
+        background-color: #3498db;
+        border: none;
+        border-radius: 4px;
+        transition: background-color 0.3s;
+        color: white;
+        font-size: 16px
+    }
+</style>
+
 <body>
     <header>
         <h1>Gym Application Confirmation</h1>
     </header>
     <p>Thank you for submitting your gym application. The information has been successfully added to the database.</p>
-    <a href="../../Admin/admin_login_form.php"><button>Back to Dashboard</button></a>
+    <a href="../../Admin/admin_login_form.php"><button>Proceed to login</button></a>
 </body>
 
 </html>
