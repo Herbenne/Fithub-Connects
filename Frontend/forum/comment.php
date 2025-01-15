7f47e07089c9def83e6675a3568dbcdb424a7edd
Reyben <somoherbenne09@gmail.com>
2025-01-15 22:59:26 +0800

    fixed forum

--- Frontend/forum/comment.php
+++ Frontend/forum/comment.php
@@ -1,21 +1,25 @@
 <div class="comment">
-  <h4><?php echo $data['name']; ?></h4>
-  <p><?php echo $data['date']; ?></p>
-  <p><?php echo $data['comment']; ?></p> <!-- Show the comment as-is -->
-
-  <!-- Display the tags -->
-  <p><strong>Tags:</strong> <?php echo htmlspecialchars($data['tags']); ?></p> <!-- Display tags here -->
-
-  <?php $reply_id = $data['id']; ?>
-  <button class="reply" onclick="reply(<?php echo $reply_id; ?>, '<?php echo $data['name']; ?>');">Reply</button>
+  <h4><?php echo htmlspecialchars($data['name']); ?></h4>
+  <p><?php echo htmlspecialchars($data['date']); ?></p>
+  <p><?php echo nl2br(htmlspecialchars($data['comment'])); ?></p>
+  <p><strong>Tags:</strong> <?php echo htmlspecialchars($data['tags']); ?></p>
 
   <?php
-  unset($datas);
-  $datas = mysqli_query($db_connection, "SELECT * FROM tb_data WHERE reply_id = $reply_id");
-  if (mysqli_num_rows($datas) > 0) {
-    foreach ($datas as $data) {
-      require 'reply.php';
-    }
+  $reply_id = $data['id'];
+  ?>
+  <button class="reply" onclick="reply(<?php echo $reply_id; ?>, '<?php echo addslashes(htmlspecialchars($data['name'])); ?>');">Reply</button>
+
+  <?php
+  // Fetch replies for this comment
+  $stmt = $db_connection->prepare("SELECT * FROM tb_data WHERE reply_id = ?");
+  $stmt->bind_param("i", $reply_id);
+  $stmt->execute();
+  $replies = $stmt->get_result();
+
+  // Loop through each reply and include `reply.php` with isolated data
+  while ($reply = $replies->fetch_assoc()) {
+    $replyData = $reply; // Pass isolated reply data
+    require 'reply.php';
   }
   ?>
 </div>
