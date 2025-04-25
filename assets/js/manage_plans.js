5ae118d8aa5ad69a1c093925ed135f9812c35b0e
Bastian Bragi Mailed Flores <108668352+B1zzle@users.noreply.github.com>
2025-04-25 14:08:59 +0800

    Adjusted the JS and CSS

--- assets/js/manage_plans.js
+++ assets/js/manage_plans.js
@@ -3,7 +3,7 @@ function editPlan(planId) {
 }
 
 function deletePlan(planId) {
-    if (confirm('Are you sure you want to delete this membership plan?')) {
+    if (confirm('Are you sure you want to delete this membership plan from the system? This action cannot be reversed.')) {
         window.location.href = `../actions/delete_plan.php?plan_id=${planId}`;
     }
 }
--- assets/js/manage_users.js
+++ assets/js/manage_users.js
@@ -3,7 +3,7 @@ function editUser(id) {
 }
 
 function deleteUser(id) {
-    if (confirm('Are you sure you want to delete this user?')) {
+    if (confirm('Are you sure you want to delete this user from the system? This action cannot be reversed.')) {
         window.location.href = `../actions/delete_user.php?id=${id}`;
     }
 }
