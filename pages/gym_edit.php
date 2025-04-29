c60aaf9d600ddbb1860d21541a673d1e4b74ff69
Bastian Bragi Mailed Flores <108668352+B1zzle@users.noreply.github.com>
2025-04-30 05:29:06 +0800

    Update gym_edit.php

--- pages/gym_edit.php
+++ pages/gym_edit.php
@@ -234,6 +234,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update_gym'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Gym - GymHub</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/mains.css">
+    <link rel="stylesheet" href="../assets/css/fithub-ui.css">
     <link rel="stylesheet" href="../assets/css/unified-theme.css">
     <link rel="stylesheet" href="../assets/css/edit_gym.css">
     <style>
